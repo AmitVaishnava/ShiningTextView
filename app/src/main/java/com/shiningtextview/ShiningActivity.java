544d752022ca456911c69bebc5ed62b83bb1d08c
AmitVaishnava <amitvaishnava@gmail.com>
2019-04-29 21:29:21 +0530

    Initial commit

---
+++ app/src/main/java/com/shiningtextview/ShiningActivity.java
@@ -0,0 +1,17 @@
+package com.shiningtextview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by avaishnava on 17/05/17.
+ */
+
+public class ShiningActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shining_view);
+    }
+}
---
+++ app/src/main/java/com/shiningtextview/ShinnyTextView.java
@@ -0,0 +1,95 @@
+package com.shiningtextview;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Shader;
+import android.graphics.drawable.PaintDrawable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.TextView;
+
+/**
+ * Created by avaishnava on 17/05/17.
+ */
+
+public class ShinnyTextView extends TextView {
+
+    private float mGradientDiameter = 0.3f;
+
+    private ValueAnimator mAnimator;
+    private float mGradientCenter;
+    private PaintDrawable mShineDrawable;
+
+    public ShinnyTextView(final Context context) {
+        this(context, null);
+    }
+
+    public ShinnyTextView(final Context context, final AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ShinnyTextView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    }
+
+    @Override
+    protected void onSizeChanged(final int w, final int h, final int oldw, final int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (!isInEditMode()) {
+            if (mAnimator != null) {
+                mAnimator.cancel();
+            }
+            mShineDrawable = new PaintDrawable();
+            mShineDrawable.setBounds(0, 0, w, h);
+            mShineDrawable.getPaint().setShader(generateGradientShader(getWidth(), 0, 0, 0));
+            mShineDrawable.getPaint().setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+
+            mAnimator = ValueAnimator.ofFloat(0, 1);
+            mAnimator.setDuration(5 * w); // custom
+            mAnimator.setRepeatCount(ValueAnimator.INFINITE);
+            mAnimator.setRepeatMode(ValueAnimator.RESTART);
+            mAnimator.setInterpolator(new LinearInterpolator()); // Custom
+            mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(final ValueAnimator animation) {
+                    final float value = animation.getAnimatedFraction();
+                    mGradientCenter = (1 + 2 * mGradientDiameter) * value - mGradientDiameter;
+                    final float gradientStart = mGradientCenter - mGradientDiameter;
+                    final float gradientEnd = mGradientCenter + mGradientDiameter;
+                    Shader shader = generateGradientShader(w*2, gradientStart, mGradientCenter, gradientEnd);
+                    mShineDrawable.getPaint().setShader(shader);
+                    invalidate();
+                }
+            });
+            mAnimator.start();
+        }
+    }
+
+    @Override
+    protected void onDraw(final Canvas canvas) {
+        super.onDraw(canvas);
+        if (!isInEditMode() && mShineDrawable != null) {
+            mShineDrawable.draw(canvas);
+        }
+    }
+
+    private Shader generateGradientShader(int width, float... positions) {
+        int[] colorRepartition = {Color.argb(255,98,185,255), Color.argb(255,245,245,245), Color.argb(255,98,185,255)};
+        return new LinearGradient(
+                0,
+                0,
+                width,
+                0,
+                colorRepartition,
+                positions,
+                Shader.TileMode.REPEAT
+        );
+    }
+}
